0c42aa37e589317e6a77657e5df5f3a4f2b3ff29
viktorasatiani <vikaasatiani@yahoo.com>
2024-09-30 14:45:46 +0400

    setting up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,60 @@
-import styled from 'styled-components';
-import GlobalStyle from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
+import { Navigate, Router, RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Account from './pages/Account';
+import Cabins from './pages/Cabins';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
+import PageNotFound from './pages/PageNotFound';
 
-const StyledApp = styled.main`
-  /* background-color: orangered; */
-  padding: 20px;
-`;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Navigate
+        replace
+        to='/dashboard'
+      />
+    ),
+    index: true,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: '/account',
+    element: <Account />,
+  },
+  {
+    path: '/bookings',
+    element: <Bookings />,
+  },
+  {
+    path: '/cabins',
+    element: <Cabins />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/settings',
+    element: <Settings />,
+  },
+  {
+    path: '/users',
+    element: <Users />,
+  },
+  {
+    path: '*',
+    element: <PageNotFound />,
+  },
+]);
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <StyledApp>
-        <Row>
-          <Row type='horizontal'>
-            <Heading as='h1'>The Wild Oasis</Heading>
-            <div>
-              <Heading as='h2'>Check in Check out</Heading>
-
-              <Button onClick={() => alert('Check in')}>Chenk In</Button>
-              <Button onClick={() => alert('check out')}>Check out</Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as='h3'>Form</Heading>
-            <form>
-              <Input
-                type='number'
-                placeholder='Number of guest'
-              />
-              <Input
-                type='number'
-                placeholder='Number of guests'
-              />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
