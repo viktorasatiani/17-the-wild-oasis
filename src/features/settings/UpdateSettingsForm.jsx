b8f9a615dc47c9b6b870645c73a5c1ecf2d1bb24
viktorasatiani <vikaasatiani@yahoo.com>
2024-10-03 15:48:15 +0400

    added fetching settings feature

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,50 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import { useSettings } from './useSettings';
+import Spinner from '../../ui/Spinner';
 
 function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      maxBookingLenght,
+      maxGuestsPerBooking,
+      minBookingLength,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  if (isLoading) return <Spinner />;
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input
+          type='number'
+          defaultValue={minBookingLength}
+          id='min-nights'
+        />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input
+          type='number'
+          defaultValue={maxBookingLenght}
+          id='max-nights'
+        />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input
+          type='number'
+          defaultValue={maxGuestsPerBooking}
+          id='max-guests'
+        />
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input
+          type='number'
+          defaultValue={breakfastPrice}
+          id='breakfast-price'
+        />
       </FormRow>
     </Form>
   );
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,13 @@
-import Heading from "../ui/Heading";
-
+import UpdateSettingsForm from '../features/settings/UpdateSettingsForm';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 function Settings() {
-  return <Heading as="h1">Update hotel settings</Heading>;
+  return (
+    <Row>
+      <UpdateSettingsForm />
+      <Heading as='h1'>Update hotel settings</Heading>
+    </Row>
+  );
 }
 
 export default Settings;
