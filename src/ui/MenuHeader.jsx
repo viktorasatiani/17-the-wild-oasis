d33bc8d93eec6191d5c70f8cb067ef99d72098e4
viktorasatiani <vikaasatiani@yahoo.com>
2024-10-24 10:28:20 +0400

    builded app header component

---
+++ src/ui/MenuHeader.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import Logout from '../features/authentication/Logout';
+import ButtonIcon from './ButtonIcon';
+import { HiOutlineUser } from 'react-icons/hi2';
+import { useNavigate } from 'react-router';
+
+const StyledMenuHeader = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+function MenuHeader() {
+  const navigate = useNavigate();
+  return (
+    <StyledMenuHeader>
+      <li>
+        <ButtonIcon onClick={() => navigate('/account')}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledMenuHeader>
+  );
+}
+
+export default MenuHeader;
