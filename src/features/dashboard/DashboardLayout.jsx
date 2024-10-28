ee229a9aa6b26633c0ad63023f25c59549008ab0
viktorasatiani <vikaasatiani@yahoo.com>
2024-10-28 14:20:47 +0400

    displayed pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import useRecentStays from './useRecentStays';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,7 +34,7 @@ function DashboardLayout() {
         cabinsCount={cabins.length}
       />
       <div>Today`s activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedstays={confirmedStays} />
       <SalesChart
         bookings={bookings}
         numDays={numDays}
