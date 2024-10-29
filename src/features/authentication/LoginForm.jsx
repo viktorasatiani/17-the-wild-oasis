09e00c14743063633df53adbac2345da13d73cfe
viktorasatiani <vikaasatiani@yahoo.com>
2024-10-29 09:14:45 +0400

    delete credentials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import UserLogin from './userLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('viktor@example.com');
-  const [password, setPassword] = useState('vikavika123');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isPending } = UserLogin();
   function handleSubmit(e) {
     e.preventDefault();
